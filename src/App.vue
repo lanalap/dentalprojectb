c7da9b34c29cdf9a52d7b30f36bc38a2ea1f6746
Qphelia <formmosite@gmail.com>
2021-07-09 14:45:20 +0200

    Deleted logo and Helloworld, +bootstrap

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,15 @@
 </template>
 
 <script>
-import carousel from './components/Carousel'
+import carousel from "./components/Carousel";
 
 export default {
-name: 'Lexicon-Dental-Clinic',
-components: {
-  home: carousel
-}
-}
+  name: "Lexicon-Dental-Clinic",
+  components: {
+    home: carousel,
+  },
+};
 </script>
 
 <style>
-
 </style>
