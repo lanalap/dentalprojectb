558e754817fa405221dd40a49205e77ac057a56a
lanalap <lana.lapitski@gmail.com>
2021-07-12 14:10:02 +0200

    lanas add

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light transparent">
+  <div class="container-fluid">
+    
+    <img src="../assets/bilder/dental_text.png" height="80" alt="dental logo">
+    
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarCenteredExample"
+      aria-controls="navbarCenteredExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" href="#">About us</a>
+        <a class="nav-link active" href="#">Services</a>
+        <a class="nav-link active" href="#">Contact</a>
+     </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
