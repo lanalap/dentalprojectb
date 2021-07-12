3769d611ce5f5c1ce0d49f07e2c91ead84eccc2b
Qphelia <formmosite@gmail.com>
2021-07-12 16:18:49 +0200

    App.vue fix

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div>
-    <AppTopNavbar/>
-    <AppNavbar/>
+    <AppTopNavbar />
+    <AppNavbar />
     <home />
-     <appOurService />
+    <appOurService />
     <appFooter />
-
   </div>
 </template>
 
 <script>
-
-import OurService from './components/ourServiceSection/OurService.vue'
-import footer from './components/footerSection/footer.vue'
-
-export default {
-  name : 'App',
-  components : {
-    appOurService : OurService,
-    appFooter: footer
-  }
-}
-</script>
-
-
 import carousel from "./components/Carousel";
-import topnavbar from './components/TopNavbar'
-import navbar from './components/Navbar'
+import topnavbar from "./components/TopNavbar";
+import navbar from "./components/Navbar";
+import OurService from "./components/ourServiceSection/OurService.vue";
+import footer from "./components/footerSection/footer.vue";
 
 export default {
   name: "Lexicon-Dental-Clinic",
@@ -34,10 +21,11 @@ export default {
     AppTopNavbar: topnavbar,
     AppNavbar: navbar,
     home: carousel,
+    appOurService: OurService,
+    appFooter: footer,
   },
 };
 </script>
 
 <style>
-
 </style>
