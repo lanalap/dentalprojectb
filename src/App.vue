950e1cb3a4074780295c19dc21cfa20f80fe588f
ricard3 <ricard36@hotmail.com>
2021-07-13 10:04:24 +0200

    infosection

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home />
+    <infosection />
   </div>
 </template>
 
 <script>
 import carousel from "./components/Carousel";
+import info from "./components/Info";
 
 export default {
   name: "Lexicon-Dental-Clinic",
   components: {
     home: carousel,
+    infosection: info,
   },
 };
 </script>
